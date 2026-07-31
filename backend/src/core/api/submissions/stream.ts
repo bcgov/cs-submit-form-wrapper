97443ebe9af98c618711fb1788bf45f2a2af2672
BrandonSharratt <brandon@highwaythreesolutions.com>
2026-07-31 15:51:41 +0000

    fix backend lint

--- backend/src/core/api/submissions/stream.ts
+++ backend/src/core/api/submissions/stream.ts
@@ -6,5 +6,6 @@ const submissionDataStream = new StreamManager();
 export const addStreamConnection = submissionStream.addConnection.bind(submissionStream);
 export const emitSubmissionsUpdate = submissionStream.emitUpdate.bind(submissionStream);
 
-export const addDataStreamConnection = submissionDataStream.addConnection.bind(submissionDataStream);
+export const addDataStreamConnection =
+  submissionDataStream.addConnection.bind(submissionDataStream);
 export const emitSubmissionDataUpdate = submissionDataStream.emitUpdate.bind(submissionDataStream);
